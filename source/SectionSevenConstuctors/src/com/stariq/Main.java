9007bed4b7f57b2ee7dc320c351827306205a891
S Tariq <sameentariq@hotmail.co.uk>
2018-01-28 16:48:54 +0000

    Changed project name

--- source/SectionSevenConstuctors/src/com/stariq/Main.java
+++ source/SectionSevenConstuctors/src/com/stariq/Main.java
@@ -3,8 +3,8 @@ package com.stariq;
 public class Main {
 
     public static void main(String[] args) {
-        //addCars();
-        //addBankAccounts();
+        addCars();
+        addBankAccounts();
         addVipCustomers();
     }
 
