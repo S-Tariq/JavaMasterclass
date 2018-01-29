f9dfb3c9fd32e7eb2c6e1a5a0f881a8080f19da1
S Tariq <sameentariq@hotmail.co.uk>
2018-01-29 22:03:59 +0000

    Started Composition

---
+++ source/SectionEightComposition/src/com/stariq/Main.java
@@ -0,0 +1,27 @@
+package com.stariq;
+
+public class Main {
+
+    public static void main(String[] args) {
+        addPCs();
+    }
+
+    public static void addPCs(){
+
+        System.out.println("\n*****");
+
+        // Adds details to Dimensions, Case, Monitor and Motherboard class.
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("220B", "Dell", "240", dimensions);
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27,
+                new Resolution(2540,1440));
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4,6,"v2.44");
+
+        // Adds details to the PC class and makes use of methods from the objects created.
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
+        thePC.getMotherboard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
+        
+    }
+}
