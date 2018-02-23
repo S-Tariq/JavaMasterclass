e710cf540c986429cf875fd71f87cfc62631c67a
S Tariq <sameentariq@hotmail.co.uk>
2018-02-23 17:06:02 +0000

    Completed Arrays

--- source/SectionNineArrays/src/com/stariq/Main.java
+++ source/SectionNineArrays/src/com/stariq/Main.java
@@ -5,26 +5,63 @@ import java.util.Scanner;
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
+    private static int[] baseData = new int[10];
 
     public static void main(String[] args) {
         initialisingArray();
         userInputtingArray();
         descendingArray();
+        extendingArray();
+    }
+
+    public static void extendingArray(){
+
+        System.out.println("\n*****");
+
+        System.out.println("Enter " + baseData.length + " integers: ");
+        getInput(baseData);
+        System.out.println("\nOriginal array: ");
+        printArray(baseData);
+
+
+        resizeArray();
+        // Initialises array with new integers by overriding old integers.
+//        System.out.println("Enter " + baseData.length + " integers: ");
+//        getInput(baseData);
+        // Keeps the old integers and extends the array by adding new integers.
+        baseData[10] = 11;
+        baseData[11] = 12;
+        System.out.println("\nExtended array: ");
+        printArray(baseData);
+
+    }
+
+    public static void resizeArray(){
+
+        // The resized array can be initialised with new integers by overriding the old ones.
+        int[] original = baseData;
+
+        baseData = new int[12];
+        for(int i = 0; i < original.length; i++){
+            baseData[i] = original[i];
+        }
     }
 
     public static void descendingArray() {
 
         System.out.println("\n*****");
 
-        int[] integersArray = getIntegers(5);
-        System.out.println("Original array:");
+        int[] integersArray = new int[5];
+        System.out.println("Enter " + integersArray.length + " integer values: ");
+        getInput(integersArray);
+        System.out.println("\nOriginal array:");
         printArray(integersArray);
-        System.out.println("Sorted array:");
-        int[] sortedArray = sortIntegers(integersArray);
+        System.out.println("\nSorted array:");
+        int[] sortedArray = sortArray(integersArray);
         printArray(sortedArray);
     }
 
-    public static int[] sortIntegers(int[] array) {
+    public static int[] sortArray(int[] array) {
 
         int[] newArray = new int[array.length];
         for (int i = 0; i < newArray.length; i++) {
@@ -51,24 +88,18 @@ public class Main {
 
         System.out.println("\n*****");
 
-        int[] myIntegers = getIntegers(5);
-
-        for(int i = 0; i < myIntegers.length; i++){
-            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
-        }
-
+        int[] myIntegers = new int[5];
+        System.out.println("Enter " + myIntegers.length + " integer values: ");
+        getInput(myIntegers);
+        printArray(myIntegers);
         System.out.println("The average is " + getAverage(myIntegers));
     }
 
-    public static int[] getIntegers(int length){
-
-        System.out.println("Enter " + length + " integer values.\r");
-        int[] array = new int[length];
+    public static int[] getInput(int[] array){
 
         for(int i = 0; i < array.length; i++){
             array[i] = scanner.nextInt();
         }
-
         return array;
     }
 
