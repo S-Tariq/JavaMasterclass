18d33d491f7e770194fd1604d005747c9dde7e92
S Tariq <sameentariq@hotmail.co.uk>
2018-02-26 23:11:36 +0000

    Added IntClass

--- source/SectionNineAutoboxingAndUnboxing/src/com/stariq/Main.java
+++ source/SectionNineAutoboxingAndUnboxing/src/com/stariq/Main.java
@@ -1,8 +1,52 @@
 package com.stariq;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        addIntClass();
+    }
+
+    public static void addIntClass(){
+
+        System.out.println("\n*****");
+
+        String[] strArray = new String[10];
+        int[] intArray = new int[10];
+
+        ArrayList<String> strArrayList = new ArrayList<String>();
+        strArrayList.add("Sam");
+
+        ArrayList<IntClass> intClassArrayList = new ArrayList<IntClass>();
+        intClassArrayList.add(new IntClass(16));
+
+        Integer integer = new Integer(12);
+        Double doubleValue = new Double(12.12);
+
+        ArrayList<Integer> intArrayList = new ArrayList<Integer>();
+        for(int i = 0; i <= 10; i++){
+            intArrayList.add(Integer.valueOf(i));
+        }
+
+        for(int i = 0; i <= 10; i++){
+            System.out.println(i + " -> " + intArrayList.get(i).intValue());
+        }
+    }
+}
+
+class IntClass{
+    private int myValue;
+
+    public IntClass(int myValue){
+        this.myValue = myValue;
+    }
+
+    public int getMyValue() {
+        return myValue;
+    }
+
+    public void setMyValue(int myValue) {
+        this.myValue = myValue;
     }
 }
