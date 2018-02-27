7b6ac2ca7540d38ac9d5b324da7c7348de2b389e
S Tariq <sameentariq@hotmail.co.uk>
2018-02-27 22:52:37 +0000

    Added Customer class

--- source/SectionNineAutoboxingAndUnboxing/src/com/stariq/Main.java
+++ source/SectionNineAutoboxingAndUnboxing/src/com/stariq/Main.java
@@ -5,7 +5,16 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        addIntClass();
+        //addIntClass();
+        addBank();
+    }
+
+    public static void addBank(){
+
+        System.out.println("\n*****");
+
+
+
     }
 
     public static void addIntClass(){
@@ -24,13 +33,33 @@ public class Main {
         Integer integer = new Integer(12);
         Double doubleValue = new Double(12.12);
 
-        ArrayList<Integer> intArrayList = new ArrayList<Integer>();
+        System.out.println("Integer: ");
+
+        // Autoboxing
+        ArrayList<Integer> myIntValues = new ArrayList<Integer>();
         for(int i = 0; i <= 10; i++){
-            intArrayList.add(Integer.valueOf(i));
+            myIntValues.add(Integer.valueOf(i));
+        }
+        // Unboxing
+        for(int i = 0; i <= 10; i++){
+            int value = myIntValues.get(i).intValue();
+            System.out.println(i + " -> " + value);
         }
 
-        for(int i = 0; i <= 10; i++){
-            System.out.println(i + " -> " + intArrayList.get(i).intValue());
+        Integer myIntValue = 16; // Integer.valueOf(16);
+        int myInt = myIntValue; // myIntValue.intValue();
+
+        System.out.println("Double: ");
+
+        // Autoboxing
+        ArrayList<Double> myDoubleValues = new ArrayList<Double>();
+        for(double i = 0.0; i <= 10.0; i+= 0.5){
+            myDoubleValues.add(Double.valueOf(i));
+        }
+        // Unboxing
+        for(int i = 0; i < myDoubleValues.size(); i++){
+            double value = myDoubleValues.get(i).doubleValue();
+            System.out.println(i + " -> " + value);
         }
     }
 }
