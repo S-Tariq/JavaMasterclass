7818d85e1de541f5d31bf8e5e0cfe985c5860b90
S Tariq <sameentariq@hotmail.co.uk>
2018-03-01 22:53:34 +0000

    Added Customer class

--- source/SectionNineLinkedList/src/com/stariq/Main.java
+++ source/SectionNineLinkedList/src/com/stariq/Main.java
@@ -3,6 +3,19 @@ package com.stariq;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        addCustomer();
+    }
+
+    public static void addCustomer(){
+
+        System.out.println("\n******");
+
+        Customer customer = new Customer("Sam", 56);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(45);
+        System.out.println("Balance for customer " + customer.getName() +
+                " is " + customer.getBalance());
+        
     }
 }
