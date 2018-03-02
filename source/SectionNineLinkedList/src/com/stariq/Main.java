ac6add9e44d5cb78d069535e10e90cc51b32663d
S Tariq <sameentariq@hotmail.co.uk>
2018-03-02 23:21:16 +0000

    Completed LinkedList Part 1

--- source/SectionNineLinkedList/src/com/stariq/Main.java
+++ source/SectionNineLinkedList/src/com/stariq/Main.java
@@ -1,5 +1,7 @@
 package com.stariq;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -16,6 +18,20 @@ public class Main {
         anotherCustomer.setBalance(45);
         System.out.println("Balance for customer " + customer.getName() +
                 " is " + customer.getBalance());
-        
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        intList.add(4);
+        intList.add(5);
+        intList.add(3);
+
+        for(int i = 0; i < intList.size(); i++){
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1,2);
+
+        for(int i = 0; i < intList.size(); i++){
+            System.out.println(i + ": " + intList.get(i));
+        }
     }
 }
