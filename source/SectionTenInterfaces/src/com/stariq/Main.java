57fc5531016895cc6d22ea284554c06e2deb91f6
S Tariq <sameentariq@hotmail.co.uk>
2018-03-18 21:10:38 +0000

    Completed Interfaces Part 1

--- source/SectionTenInterfaces/src/com/stariq/Main.java
+++ source/SectionTenInterfaces/src/com/stariq/Main.java
@@ -3,7 +3,16 @@ package com.stariq;
 public class Main {
 
     public static void main(String[] args) {
+        addDeskPhone();
 
+    }
+
+    public static void addDeskPhone(){
+        ITelephone phone;
+        phone = new DeskPhone(3456433);
+        phone.powerOn();
+        phone.callPhone(3456433);
+        phone.answer();
 
     }
 }
