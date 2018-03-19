6de3f71b877e3bee12e0aab4d44fcf64b037b177
S Tariq <sameentariq@hotmail.co.uk>
2018-03-19 21:57:16 +0000

    Added Monster class

--- source/SectionTenInterfaces/src/com/stariq/Main.java
+++ source/SectionTenInterfaces/src/com/stariq/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main(String[] args) {
         //addDeskPhone();
         //addMobilePhone();
-        addPlayer();
+        //addPlayer();
+
     }
 
     public static void addPlayer(){
