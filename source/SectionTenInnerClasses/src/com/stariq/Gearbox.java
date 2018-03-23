c595f2a1d72a3da2813040234d088fec4c8752f7
S Tariq <sameentariq@hotmail.co.uk>
2018-03-23 15:51:53 +0000

    Added methods regarding speed

--- source/SectionTenInnerClasses/src/com/stariq/Gearbox.java
+++ source/SectionTenInnerClasses/src/com/stariq/Gearbox.java
@@ -7,6 +7,7 @@ public class Gearbox {
     private ArrayList<Gear> gears = new ArrayList<Gear>();
     private int maxGears;
     private int currentGear = 0;
+    private boolean clutchIsIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
@@ -14,7 +15,35 @@ public class Gearbox {
         gears.add(neutral);
     }
 
-    public class Gear{
+    public void operateClutch(boolean in){
+        clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio){
+        if(number > 0 && number <= maxGears){
+            gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear){
+        if(newGear >= 0 && newGear < gears.size() && clutchIsIn){
+            currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected");
+        } else {
+            System.out.println("Grind");
+            currentGear = 0;
+        }
+    }
+
+    public double wheelSpeed(int revs){
+        if(clutchIsIn){
+            System.out.println("Scream");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
+    }
+
+    private class Gear{
 
         private int gearNumber;
         private double ratio;
@@ -26,6 +55,10 @@ public class Gearbox {
 
         public double driveSpeed(int revs){
             return revs * (ratio);
+        }
+
+        public double getRatio() {
+            return ratio;
         }
     }
 }
