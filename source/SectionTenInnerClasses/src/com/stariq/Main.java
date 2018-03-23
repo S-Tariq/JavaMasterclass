60191b5b508e1f5b6ace0722f2221bba371072a8
S Tariq <sameentariq@hotmail.co.uk>
2018-03-23 21:49:44 +0000

    Added info regarding Gearbox class

--- source/SectionTenInnerClasses/src/com/stariq/Main.java
+++ source/SectionTenInnerClasses/src/com/stariq/Main.java
@@ -11,7 +11,15 @@ public class Main {
         System.out.println("\n*****");
 
         Gearbox mcLaren = new Gearbox(6);
-        Gearbox.Gear first = mcLaren.new Gear(1,12.12);
-        System.out.println(first.driveSpeed(1000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
     }
 }
