c00eed950bacd23b3ab345c2fb11f28cb532ea15
S Tariq <sameentariq@hotmail.co.uk>
2018-04-03 21:05:08 +0100

    Added addSearchTree method

--- source/SectionTenAbstractClasses/src/com/stariq/Main.java
+++ source/SectionTenAbstractClasses/src/com/stariq/Main.java
@@ -6,7 +6,26 @@ public class Main {
 //        addDog();
 //        addPigeon();
 //        addPenguin();
-        addLinkedList();
+//        addLinkedList();
+        addSearchTree();
+    }
+
+    public static void addSearchTree(){
+
+        System.out.println("\n*****");
+
+        SearchTree tree = new SearchTree(null);
+        tree.traverse(tree.getRoot());
+
+        String stringData = "5 7 3 9 8 2 1 0 4 6";
+        String[] data = stringData.split(" ");
+
+        for(String s : data){
+            tree.addItem(new Node(s));
+        }
+
+        tree.traverse(tree.getRoot());
+
     }
 
     public static void addLinkedList(){
