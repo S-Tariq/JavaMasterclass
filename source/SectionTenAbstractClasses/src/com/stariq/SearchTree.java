ec5bc06297bf1171c8ccdd4660a77e6b66b2c031
S Tariq <sameentariq@hotmail.co.uk>
2018-04-03 21:04:48 +0100

    Implemented addItem method

--- source/SectionTenAbstractClasses/src/com/stariq/SearchTree.java
+++ source/SectionTenAbstractClasses/src/com/stariq/SearchTree.java
@@ -16,6 +16,41 @@ public class SearchTree implements NodeList {
     @Override
     public boolean addItem(ListItem item) {
 
+        if(this.root == null){
+            // the tree was empty, so this item becomes the head of the tree
+            this.root = item;
+            return true;
+        }
+
+        // otherwise, compare from the head of the tree
+        ListItem currentItem = this.root;
+        while(currentItem != null){
+            int comparison = currentItem.compareTo(item);
+            if(comparison < 0){
+                // item is greater, move right if possible
+                if(currentItem.next() != null){
+                    currentItem = currentItem.next();
+                } else {
+                    // there is no node to the right, so add at this point
+                    currentItem.setNext(item);
+                    return true;
+                }
+            } else if (comparison > 0){
+                // item is less, move left if possible
+                if(currentItem.previous() != null){
+                    currentItem = currentItem.previous();
+                } else {
+                    // there is no node to the left, so add at this point
+                    currentItem.setPrevious(item);
+                    return true;
+                }
+            } else {
+                // equal, so do not add
+                System.out.println(item.getValue() + " is already present");
+                return false;
+            }
+        }
+        return false;
     }
 
     @Override
