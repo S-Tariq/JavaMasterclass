959aa20356e709fdb9285e38e7068aade11f8d40
S Tariq <35509620+S-Tariq@users.noreply.github.com>
2018-07-12 14:49:36 +0100

    Added Player class

---
+++ source/SectionElevenGenerics/src/com/stariq/Player.java
@@ -0,0 +1,14 @@
+package com.stariq;
+
+public abstract class Player {
+
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
