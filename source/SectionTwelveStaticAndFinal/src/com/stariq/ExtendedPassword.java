285e1c92cd4920db1ee3366bf0d8c9920fce8770
S Tariq <35509620+S-Tariq@users.noreply.github.com>
2018-07-24 21:14:24 +0100

    Added ExtendedPassword class

---
+++ source/SectionTwelveStaticAndFinal/src/com/stariq/ExtendedPassword.java
@@ -0,0 +1,16 @@
+package com.stariq;
+
+public class ExtendedPassword extends Password{
+
+    private int decryptedPassowrd;
+
+    public ExtendedPassword(int password) {
+        super(password);
+        this.decryptedPassowrd = password;
+    }
+
+//    @Override
+//    public void storePassword() {
+//        System.out.println("Saving password as " + this.decryptedPassowrd);
+//    }
+}
