4c559f50602fb7ee84517bd4710089017cd19fcc
S Tariq <35509620+S-Tariq@users.noreply.github.com>
2018-07-24 21:14:13 +0100

    Added Password class

---
+++ source/SectionTwelveStaticAndFinal/src/com/stariq/Password.java
@@ -0,0 +1,29 @@
+package com.stariq;
+
+public class Password {
+
+    private static final int key = 784546745;
+    private final int encryptedPassword;
+
+    public Password(int password) {
+        this.encryptedPassword = encryptDecrypt(password);
+    }
+
+    private int encryptDecrypt(int password){
+        return password ^ key;
+    }
+
+    public final void storePassword(){
+        System.out.println("Saving password as " + this.encryptedPassword);
+    }
+
+    public boolean letMeIn(int password){
+        if(encryptDecrypt(password) == this.encryptedPassword){
+            System.out.println("Welcome");
+            return true;
+        } else {
+            System.out.println("Cannot come in");
+            return false;
+        }
+    }
+}
