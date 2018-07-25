e63c544761af0dbdb290391dda4473613d6ad9e8
S Tariq <35509620+S-Tariq@users.noreply.github.com>
2018-07-25 18:11:11 +0100

    Completed Collections

---
+++ source/SectionThirteenCollections/src/com/stariq/Main.java
@@ -0,0 +1,23 @@
+package com.stariq;
+
+public class Main {
+
+    public static void main(String[] args) {
+        addTheatre();
+    }
+
+    public static void addTheatre(){
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+        theatre.getSeats();
+        if(theatre.reserveSeat("H11")){
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+        if(theatre.reserveSeat("H11")){
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+    }
+}
